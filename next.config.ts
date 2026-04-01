ff6f2b63de7c82a1b5b9a236a59eade451399234
Eduardo <eduardogabrielmendoza@gmail.com>
2026-03-31 21:00:12 -0300

    Disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
